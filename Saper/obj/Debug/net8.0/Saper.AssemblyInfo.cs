9eb0faf4e951c66f8e432c7f9fcaf1048b7debe7
Sebastian Marcinkowski <kontakt@sebastianmarcinkowski.pl>
2024-03-03 15:23:14 +0100

    I maked logic error? correct? -> for (int j = Math.Max(0, x - 1); j <= Math.Min(width - 1, x + 1); j++)

--- Saper/obj/Debug/net8.0/Saper.AssemblyInfo.cs
+++ Saper/obj/Debug/net8.0/Saper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Saper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a41aa5d927fe4b9ceab92d934e319e54a65bfd5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59d6e7dc6c39bec6d1146683cf95d73a2c0a5c51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Saper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Saper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
